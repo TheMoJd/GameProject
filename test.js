2df54707bf70536886530786fe983bada88319f4
MoetazJ <jaouedmoetaz@gmail.com>
2023-12-30 15:16:56 +0100

    V1

---
+++ test.js
@@ -0,0 +1,25 @@
+//Object
+/*
+let person = {
+    name: "Mosh",
+    age: 30
+}
+person.name = "John";
+console.log(person.name);*/
+
+/*
+// array
+let selectedColors = ['red', 'blue'];
+selectedColors[2] = 1;
+console.log(selectedColors);
+
+let person = {
+    name: "Mosh",
+    age: 30
+}
+
+function greet(person) {
+    console.log('Hello ' + person.name + ' ' + person.age);
+}
+
+greet(person);*/
